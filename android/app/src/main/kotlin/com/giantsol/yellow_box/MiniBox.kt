72da4b7eee4ac11e39425a89c1bc6664d3882a4a
OreOreDa <olivier.le-quellec@renault.com>
2022-03-25 22:04:21 +0100

    minibox fix

---
+++ android/app/src/main/kotlin/com/giantsol/yellow_box/MiniBox.kt
@@ -0,0 +1,400 @@
+package com.giantsol.yellow_box
+
+import android.animation.*
+import android.content.Context
+import android.graphics.PixelFormat
+import android.graphics.Point
+import android.graphics.PointF
+import android.graphics.Rect
+import android.os.Build
+import android.util.TypedValue
+import android.view.*
+import android.view.animation.OvershootInterpolator
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.sin
+
+class MiniBox(private val context: Context,
+              private val callback: Callback): View.OnKeyListener {
+
+    interface Callback {
+        fun stopMiniBox()
+        fun addWord(word: String)
+    }
+
+    private val wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    private val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+    private val bgView = LayoutInflater.from(context).inflate(R.layout.view_mini_box_bg, null) as KeyInterceptRelativeLayout
+
+    private val closeView = bgView.findViewById<View>(R.id.close)
+    private val closeViewRect = Rect()
+
+    private val wordEditorView = bgView.findViewById<View>(R.id.word_editor)
+    private val editorView = wordEditorView.findViewById<EditText>(R.id.editor)
+    private val addButton = wordEditorView.findViewById<View>(R.id.add)
+
+    private val miniBoxView = LayoutInflater.from(context).inflate(R.layout.view_mini_box, null)
+    private lateinit var miniBoxViewLp: WindowManager.LayoutParams
+    private val miniBoxViewSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48f, context.resources.displayMetrics).toInt()
+    private val miniBoxViewInset = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4f, context.resources.displayMetrics).toInt()
+
+    private val windowSize = Point().apply { wm.defaultDisplay.getSize(this) }
+
+    private val pvhX = PropertyValuesHolder.ofInt("x", 0)
+    private val pvhY = PropertyValuesHolder.ofInt("y", 0)
+    private val springBackAnimator = ValueAnimator.ofPropertyValuesHolder(pvhX, pvhY).apply {
+        duration = 400
+        interpolator = OvershootInterpolator()
+    }
+
+    private val showWordEditorAnimator = ObjectAnimator.ofFloat(wordEditorView, "x", 0f).apply {
+        duration = 400
+        interpolator = OvershootInterpolator()
+    }
+
+    private val hideWordEditorAnimator = ObjectAnimator.ofFloat(wordEditorView, "x", 0f).apply {
+        duration = 400
+        interpolator = OvershootInterpolator()
+    }
+
+    private val wordAddedAnimator = ValueAnimator.ofFloat(0f).apply {
+        duration = 400
+        interpolator = FastOutSlowInInterpolator()
+    }
+
+    private val statusBarHeight: Int
+
+    val isDestroyed get() = !miniBoxView.isAttachedToWindow
+
+    init {
+        initBgView()
+        initMiniBoxView()
+        initAnimators()
+
+        val statusResourceId = context.resources.getIdentifier("status_bar_height", "dimen", "android")
+        statusBarHeight = if (statusResourceId > 0) {
+            context.resources.getDimensionPixelSize(statusResourceId)
+        } else {
+            0
+        }
+    }
+
+    private fun initBgView() {
+        val lp = getWindowParams(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT)
+        lp.flags = lp.flags and WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE.inv()
+        wm.addView(bgView, lp)
+
+        bgView.setKeyInterceptListener(this)
+        bgView.setOnClickListener {
+            hideWordEditor()
+        }
+
+        addButton.setOnClickListener {
+            addWordIfPossible()
+        }
+
+        editorView.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                pullUpWordEditorForInputIfNeeded()
+            }
+        }
+        editorView.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                addButton.performClick()
+                true
+            } else {
+                false
+            }
+        }
+
+        bgView.visibility = View.GONE
+        closeView.visibility = View.GONE
+        wordEditorView.visibility = View.INVISIBLE
+    }
+
+    private fun initMiniBoxView() {
+        miniBoxViewLp = getWindowParams(miniBoxViewSize, miniBoxViewSize)
+        miniBoxViewLp.x = windowSize.x - miniBoxViewSize + miniBoxViewInset
+        miniBoxViewLp.y = (windowSize.y - miniBoxViewSize - statusBarHeight) / 2
+        wm.addView(miniBoxView, miniBoxViewLp)
+
+        miniBoxView.setOnTouchListener(object: View.OnTouchListener {
+            private val touchSlop = ViewConfiguration.get(context).scaledTouchSlop
+            private val touchDownPoint = PointF()
+            private val touchDownRawPoint = PointF()
+            private var isDragging = false
+
+            override fun onTouch(v: View, event: MotionEvent): Boolean {
+                val x = event.x
+                val y = event.y
+                val rawX = event.rawX
+                val rawY = event.rawY
+
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        touchDownPoint.set(x, y)
+                        touchDownRawPoint.set(rawX, rawY)
+                        isDragging = false
+                    }
+                    MotionEvent.ACTION_MOVE -> {
+                        val moveDiffX = rawX - touchDownRawPoint.x
+                        val moveDiffY = rawY - touchDownRawPoint.y
+                        if (abs(moveDiffX) + abs(moveDiffY) > touchSlop && !isDragging) {
+                            isDragging = true
+                        }
+
+                        if (isDragging) {
+                            onDragging(rawX - touchDownPoint.x, rawY - touchDownPoint.y)
+                        }
+                    }
+                    MotionEvent.ACTION_UP,
+                    MotionEvent.ACTION_CANCEL -> {
+                        if (isDragging) {
+                            onStopDragging()
+                        } else if (event.action == MotionEvent.ACTION_UP) {
+                            v.performClick()
+                        }
+                        isDragging = false
+                    }
+                }
+                return true
+            }
+        })
+
+        miniBoxView.setOnClickListener {
+            toggleWordEditor()
+        }
+    }
+
+    private fun initAnimators() {
+        springBackAnimator.addUpdateListener {
+            val x: Int = it.getAnimatedValue("x") as Int
+            val y: Int = it.getAnimatedValue("y") as Int
+            miniBoxViewLp.x = x
+            miniBoxViewLp.y = y
+            wm.updateViewLayout(miniBoxView, miniBoxViewLp)
+        }
+
+        springBackAnimator.addListener(object: AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator, isReverse: Boolean) {
+                miniBoxView.scaleX = 1f
+                miniBoxView.scaleY = 1f
+                closeView.isActivated = false
+                closeView.visibility = View.GONE
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                bgView.visibility = View.GONE
+            }
+        })
+
+        showWordEditorAnimator.addListener(object: AnimatorListenerAdapter() {
+            private var isCancelled = false
+
+            override fun onAnimationStart(animation: Animator) {
+                bgView.visibility = View.VISIBLE
+                wordEditorView.visibility = View.VISIBLE
+                closeView.visibility = View.GONE
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+                isCancelled = true
+            }
+
+            override fun onAnimationEnd(animation: Animator?, isReverse: Boolean) {
+                if (!isCancelled) {
+                    editorView.requestFocus()
+                    imm.showSoftInput(editorView, InputMethodManager.SHOW_IMPLICIT)
+                }
+                isCancelled = false
+            }
+        })
+
+        hideWordEditorAnimator.addListener(object: AnimatorListenerAdapter() {
+            private var isCancelled = false
+
+            override fun onAnimationStart(animation: Animator, isReverse: Boolean) {
+                editorView.clearFocus()
+                imm.hideSoftInputFromWindow(editorView.windowToken, InputMethodManager.HIDE_IMPLICIT_ONLY)
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+                isCancelled = true
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                if (!isCancelled) {
+                    bgView.visibility = View.GONE
+                    wordEditorView.visibility = View.INVISIBLE
+                    editorView.text = null
+                }
+                isCancelled = false
+            }
+        })
+
+        wordAddedAnimator.addUpdateListener {
+            val scale = (1 + 0.2f * sin(it.animatedFraction * Math.PI * 2)).toFloat()
+            miniBoxView.scaleX = scale
+            miniBoxView.scaleY = scale
+        }
+    }
+
+    private fun getWindowParams(width: Int, height: Int): WindowManager.LayoutParams {
+        val type = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+        else
+            WindowManager.LayoutParams.TYPE_PHONE
+
+        return WindowManager.LayoutParams(
+            width, height, type,
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+                or WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+            PixelFormat.TRANSLUCENT
+        ).apply {
+            gravity = Gravity.TOP or Gravity.START
+        }
+    }
+
+    private fun onDragging(x: Float, y: Float) {
+        bgView.visibility = View.VISIBLE
+        closeView.visibility = View.VISIBLE
+        hideWordEditor()
+
+        miniBoxView.scaleX = 1.2f
+        miniBoxView.scaleY = 1.2f
+        miniBoxViewLp.x = x.toInt()
+        miniBoxViewLp.y = y.toInt()
+        wm.updateViewLayout(miniBoxView, miniBoxViewLp)
+
+        if (closeViewRect.isEmpty) {
+            closeView.getGlobalVisibleRect(closeViewRect)
+        }
+
+        closeView.isActivated = closeViewRect.intersects(miniBoxViewLp.x, miniBoxViewLp.y,
+            miniBoxViewLp.x + miniBoxViewSize, miniBoxViewLp.y + miniBoxViewSize)
+    }
+
+    private fun onStopDragging() {
+        if (closeView.isActivated) {
+            callback.stopMiniBox()
+        } else {
+            springBackToWall()
+        }
+    }
+
+    private fun springBackToWall() {
+        val finalX: Int = if (miniBoxViewLp.x >= windowSize.x / 2) {
+            windowSize.x - miniBoxViewSize + miniBoxViewInset
+        } else {
+            -miniBoxViewInset
+        }
+
+        val finalY: Int = max(0, min(miniBoxViewLp.y, windowSize.y - miniBoxViewSize - statusBarHeight))
+
+        pvhX.setIntValues(miniBoxViewLp.x, finalX)
+        pvhY.setIntValues(miniBoxViewLp.y, finalY)
+        springBackAnimator.apply {
+            cancel()
+            start()
+        }
+    }
+
+    private fun toggleWordEditor() {
+        if (isWordEditorShown()) {
+            hideWordEditor()
+        } else {
+            showWordEditor()
+        }
+    }
+
+    private fun isWordEditorShown() = wordEditorView.visibility == View.VISIBLE
+
+    private fun hideWordEditor() {
+        if (!isWordEditorShown() || hideWordEditorAnimator.isRunning) {
+            return
+        }
+
+        showWordEditorAnimator.cancel()
+        hideWordEditorAnimator.apply {
+            if (miniBoxViewLp.x > windowSize.x / 2) {
+                setFloatValues((wordEditorView.layoutParams as ViewGroup.MarginLayoutParams).marginStart - miniBoxViewSize.toFloat(), windowSize.x.toFloat())
+            } else {
+                setFloatValues((wordEditorView.layoutParams as ViewGroup.MarginLayoutParams).marginStart.toFloat(), -wordEditorView.width.toFloat())
+            }
+            start()
+        }
+    }
+
+    private fun showWordEditor() {
+        if (isWordEditorShown() || showWordEditorAnimator.isRunning) {
+            return
+        }
+
+        hideWordEditorAnimator.cancel()
+        showWordEditorAnimator.apply {
+            if (miniBoxViewLp.x > windowSize.x / 2) {
+                setFloatValues(windowSize.x.toFloat(), (wordEditorView.layoutParams as ViewGroup.MarginLayoutParams).marginStart - miniBoxViewSize.toFloat())
+            } else {
+                setFloatValues(-wordEditorView.width.toFloat(), (wordEditorView.layoutParams as ViewGroup.MarginLayoutParams).marginStart.toFloat())
+            }
+            start()
+        }
+        wordEditorView.y = miniBoxViewLp.y.toFloat()
+    }
+
+    private fun addWordIfPossible() {
+        val word = editorView.text.toString()
+        if (word.isNotEmpty()) {
+            callback.addWord(word)
+            editorView.text = null
+
+            wordAddedAnimator.apply {
+                cancel()
+                start()
+            }
+        }
+    }
+
+    private fun pullUpWordEditorForInputIfNeeded() {
+        if (wordEditorView.y > windowSize.y / 4) {
+            val animator = ValueAnimator.ofFloat(wordEditorView.y, windowSize.y / 4f).apply {
+                duration = 400
+                interpolator = OvershootInterpolator()
+            }
+            animator.addUpdateListener {
+                val value = it.animatedValue as Float
+                wordEditorView.y = value
+                miniBoxViewLp.y = value.toInt()
+                wm.updateViewLayout(miniBoxView, miniBoxViewLp)
+            }
+            animator.start()
+        }
+    }
+
+    override fun onKey(v: View, keyCode: Int, event: KeyEvent): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.action == KeyEvent.ACTION_UP && isWordEditorShown()) {
+            hideWordEditor()
+            return true
+        }
+
+        return false
+    }
+
+    fun destroy() {
+        springBackAnimator.removeAllListeners()
+        showWordEditorAnimator.removeAllListeners()
+        hideWordEditorAnimator.removeAllListeners()
+        wordAddedAnimator.removeAllListeners()
+
+        if (miniBoxView.isAttachedToWindow) {
+            wm.removeView(miniBoxView)
+            wm.removeView(bgView)
+        }
+    }
+}
---
+++ android/app/src/main/kotlin/com/giantsol/yellow_box/MiniBoxService.kt
@@ -0,0 +1,244 @@
+package com.giantsol.yellow_box
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.content.res.Configuration
+import android.graphics.PixelFormat
+import android.net.Uri
+import android.os.Build
+import android.os.IBinder
+import android.provider.Settings
+import android.view.View
+import android.view.WindowManager
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+
+
+class MiniBoxService : Service(), MiniBox.Callback {
+
+    companion object {
+        const val ACTION_START_MINI_BOX = "action.start.mini.box"
+        const val ACTION_PAUSE_MINI_BOX = "action.pause.mini.box"
+        const val ACTION_RESUME_MINI_BOX = "action.resume.mini.box"
+        const val ACTION_STOP_MINI_BOX = "action.stop.mini.box"
+
+        private const val NOTIFICATION_ID = 1001
+        private const val NOTIFICATION_CHANNEL_ID = "minibox.notification.channel"
+
+        private const val REQUEST_CODE_PAUSE_BUTTON = 0
+        private const val REQUEST_CODE_STOP_BUTTON = 1
+        private const val REQUEST_CODE_RESUME_BUTTON = 2
+    }
+
+    private var miniBox: MiniBox? = null
+
+    private lateinit var notificationManager: NotificationManager
+    private val notificationBuilder by lazy {
+        NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notification_mini_box)
+            .setColor(ContextCompat.getColor(this, R.color.defaultThemeLightColor))
+            .setContentIntent(PendingIntent.getActivity(this, 0,
+                Intent(this, MainActivity::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                }, PendingIntent.FLAG_IMMUTABLE))
+            .setColorized(true)
+            .setStyle(NotificationCompat.DecoratedCustomViewStyle())
+            .setOngoing(true)
+    }
+
+    private var isTerminating = false
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        createNotificationChannel()
+
+        if (requestOverlayPermissionIfNeeded()) {
+            terminate()
+            return
+        }
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.mini_box_notification_channel_name)
+            val descriptionText = getString(R.string.mini_box_notification_channel_description)
+            val importance = NotificationManager.IMPORTANCE_LOW
+            val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance).apply { description = descriptionText }
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun terminate() {
+        stopForeground(true)
+        stopSelf()
+        miniBox?.destroy() // should remove views from window
+        miniBox = null
+        isTerminating = true
+    }
+
+    private fun requestOverlayPermissionIfNeeded(): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !canDrawOverlays(this)) {
+            try {
+                val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:${packageName}")).apply {
+                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                }
+                startActivity(intent)
+                return true
+            } catch (ignored: Exception) { }
+            terminate()
+        }
+        return false
+    }
+
+    private fun canDrawOverlays(context: Context): Boolean {
+        return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            true
+        } else if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            // with Android 8.1 (API 27) was released that it should contain the fix
+            Settings.canDrawOverlays(context)
+        } else {
+            // this case version is between 6.0 (API 23) and 8.0 (API 26)
+            if (Settings.canDrawOverlays(context)) {
+                true
+            } else {
+                // In Android 8.0 (API 26), when user successfully grant permission and return to the app by pressing back button,
+                // method canDrawOverlays() still return false, until user don't close the app and open it again or just choose it in recent apps dialog.
+                canDrawOverlaysWorkAround(context)
+            }
+        }
+    }
+
+    /**
+     * Android O에서 다른 앱 위에 그리기를 on으로 하고 onActivityResult()에서 권한 체크를 할 때
+     * 타이밍 이슈로 Settings.canDrawOverlays()가 false를 리턴한다.
+     * 그에 대한 해결책으로 TYPE_APPLICATION_OVERLAY param을 갖는 transparent view를 add/remove하여
+     * 권한이 있는 경우에는 true, 권한이 없는 경우에는 false를 리턴하도록 하여 Overlay 권한을 체크한다.
+     * 참고: https://stackoverflow.com/questions/46173460
+     */
+    private fun canDrawOverlaysWorkAround(context: Context): Boolean {
+        try {
+            val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+            val viewToAdd = View(context)
+            val type = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+            } else {
+                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT
+            }
+            val params = WindowManager.LayoutParams(0, 0, type,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSPARENT)
+            viewToAdd.layoutParams = params
+            windowManager.addView(viewToAdd, params)
+            windowManager.removeView(viewToAdd)
+            return true
+        } catch (e: Exception) {
+            return false
+        }
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent) {
+        super.onTaskRemoved(rootIntent)
+        terminate()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        terminate()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (intent?.action == null || isTerminating) {
+            return START_NOT_STICKY
+        }
+
+        when (intent.action!!) {
+            ACTION_START_MINI_BOX -> startMiniBox()
+            ACTION_PAUSE_MINI_BOX -> pauseMiniBox()
+            ACTION_RESUME_MINI_BOX -> startMiniBox()
+            ACTION_STOP_MINI_BOX -> terminate()
+        }
+
+        return START_NOT_STICKY
+    }
+
+    private fun startMiniBox() {
+        miniBox?.destroy()
+
+        startForeground(NOTIFICATION_ID, createStartedNotification())
+        miniBox = MiniBox(this, this)
+    }
+
+    private fun pauseMiniBox() {
+        miniBox?.destroy()
+        notificationManager.notify(NOTIFICATION_ID, createPausedNotification())
+    }
+
+    private fun createStartedNotification(): Notification {
+        val view = RemoteViews(packageName, R.layout.mini_box_notification)
+        val pausePendingIntent = PendingIntent.getService(
+            this, REQUEST_CODE_PAUSE_BUTTON,
+            Intent(this, MiniBoxService::class.java).apply { action = ACTION_PAUSE_MINI_BOX },
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        view.setOnClickPendingIntent(R.id.pause_or_resume_button, pausePendingIntent)
+
+        val stopPendingIntent = PendingIntent.getService(
+            this, REQUEST_CODE_STOP_BUTTON,
+            Intent(this, MiniBoxService::class.java).apply { action = ACTION_STOP_MINI_BOX },
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        view.setOnClickPendingIntent(R.id.stop_button, stopPendingIntent)
+
+        view.setTextViewText(R.id.title, getText(R.string.notification_title_started))
+        view.setTextViewText(R.id.pause_or_resume_button, getText(R.string.notification_pause))
+
+        return notificationBuilder.setCustomContentView(view).build()
+    }
+
+    private fun createPausedNotification(): Notification {
+        val view = RemoteViews(packageName, R.layout.mini_box_notification)
+        val resumePendingIntent = PendingIntent.getService(
+            this, REQUEST_CODE_RESUME_BUTTON,
+            Intent(this, MiniBoxService::class.java).apply { action = ACTION_RESUME_MINI_BOX },
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        view.setOnClickPendingIntent(R.id.pause_or_resume_button, resumePendingIntent)
+
+        val stopPendingIntent = PendingIntent.getService(
+            this, REQUEST_CODE_STOP_BUTTON,
+            Intent(this, MiniBoxService::class.java).apply { action = ACTION_STOP_MINI_BOX },
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        view.setOnClickPendingIntent(R.id.stop_button, stopPendingIntent)
+
+        view.setTextViewText(R.id.title, getText(R.string.notification_title_paused))
+        view.setTextViewText(R.id.pause_or_resume_button, getText(R.string.notification_resume))
+
+        return notificationBuilder.setCustomContentView(view).build()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        if (miniBox?.isDestroyed == false) {
+            miniBox?.destroy()
+            miniBox = MiniBox(this, this)
+        }
+    }
+
+    override fun stopMiniBox() {
+        terminate()
+    }
+
+    override fun addWord(word: String) {
+        AppMethodCallHandler.deliverMiniBoxWord(this, word)
+    }
+}
